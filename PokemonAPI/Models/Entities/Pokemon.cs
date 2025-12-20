2a15d341c7550628213d05693aa03ec6cd1b66e1
eliasAxinicuilteco_h@outlook <eliasAxinicuilteco_h@outlook.com>
2025-12-19 21:16:46 -0600

    se agreó la entidad y la migracion para la base de datos

---
+++ PokemonAPI/Models/Entities/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿namespace PokemonAPI.Models.Entities
+{
+    public class Pokemon
+    {
+        public int Id { get; set; } = default!;
+        public string Name { get; set; } = string.Empty;
+        public int Power { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Nature { get; set; } = string.Empty;
+    }
+
+}
